7f7178f2e49caa36adbf8576f49727c3c59c6bbe
Metroxtt <cbrionesmarkang@uss.edu.pe>
2026-07-20 22:29:08 -0500

    feat(H2): AST - nodos del arbol de sintaxis para SELECT, WHERE, expresiones

---
+++ internal/parser/ast.go
@@ -0,0 +1,71 @@
+package parser
+
+type NodeType int
+
+const (
+	NodeSelect NodeType = iota
+	NodeExpr
+	NodeColumnRef
+	NodeStringLit
+	NodeNumberLit
+	NodeBoolLit
+	NodeNullLit
+	NodeBinaryOp
+	NodeComparison
+)
+
+type Node interface {
+	Type() NodeType
+}
+
+type SelectNode struct {
+	Columns []Node   // columnas a mostrar (* o lista)
+	Table   string   // nombre de la tabla
+	Where   Node     // condición WHERE (nil si no hay)
+}
+
+func (n *SelectNode) Type() NodeType { return NodeSelect }
+
+type ColumnRefNode struct {
+	Name string
+}
+
+func (n *ColumnRefNode) Type() NodeType { return NodeColumnRef }
+
+type StringLitNode struct {
+	Value string
+}
+
+func (n *StringLitNode) Type() NodeType { return NodeStringLit }
+
+type NumberLitNode struct {
+	Value string
+}
+
+func (n *NumberLitNode) Type() NodeType { return NodeNumberLit }
+
+type BoolLitNode struct {
+	Value bool
+}
+
+func (n *BoolLitNode) Type() NodeType { return NodeBoolLit }
+
+type NullLitNode struct{}
+
+func (n *NullLitNode) Type() NodeType { return NodeNullLit }
+
+type BinaryOpNode struct {
+	Op    string // AND, OR
+	Left  Node
+	Right Node
+}
+
+func (n *BinaryOpNode) Type() NodeType { return NodeExpr }
+
+type ComparisonNode struct {
+	Op    string // =, <>, <, >, <=, >=
+	Left  Node
+	Right Node
+}
+
+func (n *ComparisonNode) Type() NodeType { return NodeComparison }
