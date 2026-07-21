e5d763dc84ad005c1701dc5cae4413825aff70af
Metroxtt <cbrionesmarkang@uss.edu.pe>
2026-07-20 22:00:34 -0500

    feat(H2): lexer - tokenizador SQL con soporte para SELECT, WHERE, operadores

---
+++ internal/lexer/lexer.go
@@ -0,0 +1,176 @@
+package lexer
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+var keywords = map[string]TokenType{
+	"SELECT": TokenSelect, "FROM": TokenFrom,
+	"WHERE": TokenWhere, "AND": TokenAnd, "OR": TokenOr,
+}
+
+type Lexer struct {
+	input  string
+	pos    int
+	line   int
+	col    int
+	errors []string
+}
+
+func New(input string) *Lexer {
+	return &Lexer{input: input, line: 1, col: 1}
+}
+
+func (l *Lexer) Errors() []string {
+	return l.errors
+}
+
+func (l *Lexer) peek() rune {
+	if l.pos >= len(l.input) {
+		return 0
+	}
+	return rune(l.input[l.pos])
+}
+
+func (l *Lexer) advance() rune {
+	ch := rune(l.input[l.pos])
+	l.pos++
+	if ch == '\n' {
+		l.line++
+		l.col = 1
+	} else {
+		l.col++
+	}
+	return ch
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.pos < len(l.input) {
+		ch := rune(l.input[l.pos])
+		if ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' {
+			l.advance()
+		} else {
+			break
+		}
+	}
+}
+
+func (l *Lexer) readIdent() string {
+	start := l.pos - 1
+	for l.pos < len(l.input) {
+		ch := rune(l.input[l.pos])
+		if unicode.IsLetter(ch) || unicode.IsDigit(ch) || ch == '_' {
+			l.advance()
+		} else {
+			break
+		}
+	}
+	return l.input[start:l.pos]
+}
+
+func (l *Lexer) readNumber() string {
+	start := l.pos - 1
+	hasDot := false
+	for l.pos < len(l.input) {
+		ch := rune(l.input[l.pos])
+		if unicode.IsDigit(ch) {
+			l.advance()
+		} else if ch == '.' && !hasDot {
+			hasDot = true
+			l.advance()
+		} else {
+			break
+		}
+	}
+	return l.input[start:l.pos]
+}
+
+func (l *Lexer) readString() string {
+	start := l.pos
+	for l.pos < len(l.input) {
+		ch := l.advance()
+		if ch == '\'' {
+			return l.input[start : l.pos-1]
+		}
+	}
+	l.errors = append(l.errors, fmt.Sprintf("cadena sin cerrar en línea %d", l.line))
+	return l.input[start:]
+}
+
+func (l *Lexer) NextToken() Token {
+	l.skipWhitespace()
+
+	if l.pos >= len(l.input) {
+		return Token{Type: TokenEOF, Line: l.line, Column: l.col}
+	}
+
+	line, col := l.line, l.col
+	ch := l.advance()
+
+	switch {
+	case unicode.IsLetter(ch) || ch == '_':
+		word := l.readIdent()
+		upper := strings.ToUpper(word)
+		if tokType, ok := keywords[upper]; ok {
+			return Token{Type: tokType, Literal: upper, Line: line, Column: col}
+		}
+		return Token{Type: TokenIdent, Literal: word, Line: line, Column: col}
+
+	case unicode.IsDigit(ch):
+		num := l.readNumber()
+		return Token{Type: TokenNumber, Literal: num, Line: line, Column: col}
+
+	case ch == '\'':
+		str := l.readString()
+		return Token{Type: TokenString, Literal: str, Line: line, Column: col}
+
+	case ch == '*':
+		return Token{Type: TokenStar, Line: line, Column: col}
+	case ch == '(':
+		return Token{Type: TokenLParen, Line: line, Column: col}
+	case ch == ')':
+		return Token{Type: TokenRParen, Line: line, Column: col}
+	case ch == ',':
+		return Token{Type: TokenComma, Line: line, Column: col}
+
+	case ch == '=':
+		return Token{Type: TokenEq, Line: line, Column: col}
+
+	case ch == '<':
+		if l.peek() == '>' {
+			l.advance()
+			return Token{Type: TokenNeq, Line: line, Column: col}
+		}
+		if l.peek() == '=' {
+			l.advance()
+			return Token{Type: TokenLe, Line: line, Column: col}
+		}
+		return Token{Type: TokenLt, Line: line, Column: col}
+
+	case ch == '>':
+		if l.peek() == '=' {
+			l.advance()
+			return Token{Type: TokenGe, Line: line, Column: col}
+		}
+		return Token{Type: TokenGt, Line: line, Column: col}
+
+	default:
+		l.errors = append(l.errors, fmt.Sprintf("carácter inesperado %q en línea %d, columna %d", ch, line, col))
+		return Token{Type: TokenEOF, Line: line, Column: col}
+	}
+}
+
+func Tokenize(input string) ([]Token, []string) {
+	lexer := New(input)
+	var tokens []Token
+	for {
+		tok := lexer.NextToken()
+		tokens = append(tokens, tok)
+		if tok.Type == TokenEOF {
+			break
+		}
+	}
+	return tokens, lexer.Errors()
+}
---
+++ internal/lexer/token.go
@@ -0,0 +1,65 @@
+package lexer
+
+import "fmt"
+
+type TokenType int
+
+const (
+	// Literales
+	TokenIdent    TokenType = iota // nombre de columna/tabla
+	TokenNumber                    // 42, 3.14
+	TokenString                    // 'texto'
+
+	// Palabras clave
+	TokenSelect // SELECT
+	TokenFrom   // FROM
+	TokenWhere  // WHERE
+	TokenAnd    // AND
+	TokenOr     // OR
+
+	// Operadores
+	TokenEq    // =
+	TokenNeq   // <>
+	TokenLt    // <
+	TokenGt    // >
+	TokenLe    // <=
+	TokenGe    // >=
+	TokenStar  // *
+	TokenLParen // (
+	TokenRParen // )
+	TokenComma  // ,
+
+	// Especiales
+	TokenEOF // fin de entrada
+)
+
+var tokenNames = map[TokenType]string{
+	TokenIdent: "IDENT", TokenNumber: "NUMBER", TokenString: "STRING",
+	TokenSelect: "SELECT", TokenFrom: "FROM", TokenWhere: "WHERE",
+	TokenAnd: "AND", TokenOr: "OR",
+	TokenEq: "=", TokenNeq: "<>", TokenLt: "<", TokenGt: ">",
+	TokenLe: "<=", TokenGe: ">=", TokenStar: "*",
+	TokenLParen: "(", TokenRParen: ")", TokenComma: ",",
+	TokenEOF: "EOF",
+}
+
+func (t TokenType) String() string {
+	if name, ok := tokenNames[t]; ok {
+		return name
+	}
+	return "UNKNOWN"
+}
+
+type Token struct {
+	Type    TokenType
+	Literal string
+	Line    int
+	Column  int
+}
+
+func (t Token) String() string {
+	if t.Literal != "" {
+		return fmt.Sprintf("%s(%q)@%d:%d", t.Type, t.Literal, t.Line, t.Column)
+	}
+	return fmt.Sprintf("%s@%d:%d", t.Type, t.Line, t.Column)
+}
